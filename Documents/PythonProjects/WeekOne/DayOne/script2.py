20c7591a6d1cf56edb11b17e6da424daa3e66608
Maffinz <williyaaj@gmail.com>
2017-08-06 21:54:12 -0500

    isPermutate added

---
+++ Documents/PythonProjects/WeekOne/DayOne/script2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def isPermutate(str_one,str_two):
+    #Takes X to solve the problem
+    #str_one and str_two are same size
+
+    for i in xrange(len(str_one)):
+        seek = str_one[i]
+        found = False
+        for x in xrange(len(str_two)):
+            if seek == str_two[x]:
+                found = True
+                break
+        if found == False:
+            return False
+    return True)
